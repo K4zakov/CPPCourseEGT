be4c7b5070e2cc389b7f26b25e859aef24b175fd
K4zakov <156944367+K4zakov@users.noreply.github.com>
2024-02-15 23:27:24 +0200

    Task is the same as task 2 but we change 1 value

---
+++ SDL_Task3_homework/Shapes.cpp
@@ -0,0 +1,18 @@
+#include "Shapes.h"
+
+
+
+
+void Shapes::drawHexagon(SDL_Renderer* gameRenderer, int x, int y, int side) {
+
+	const int sides = 6; //5 is to draw a pentagon but we can change it to draw any polygon
+	const double radius = side / (2 * sin(M_PI / 25)); 
+	const double angles = 2 * M_PI / sides;
+
+	for (int i = 0; i < sides; i++) {
+		double angle1 = angles * i;
+		double angle2 = angles * (i + 1);
+		SDL_RenderDrawLine(gameRenderer, x + radius * cos(angle1), y + radius * sin(angle1), x + radius * cos(angle2), y + radius * sin(angle2));
+	}
+
+}
---
+++ SDL_Task3_homework/Shapes.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Game.h"
+
+class Shapes {
+
+public:
+
+	static void drawHexagon(SDL_Renderer* gameRenderer, int x, int y, int r);
+
+};
+
+
