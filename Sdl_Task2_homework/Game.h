5786fa24c26fcfa455c3f1f388b9b868f000aeab
K4zakov <156944367+K4zakov@users.noreply.github.com>
2024-02-15 18:19:52 +0200

    Add files via upload

---
+++ Sdl_Task2_homework/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <SDL.h>
+#include "Shapes.h"
+
+using namespace std;
+
+class Game {
+
+public:
+
+    Game();
+    ~Game();
+    bool init(const char* title, int xpos, int ypos, int width, int height, int flags);
+    void render();
+    void update();
+    void handleEvents();
+    void clean();
+    bool isRunning();
+
+private:
+
+    SDL_Window* gameWindow = NULL;
+    SDL_Renderer* gameRenderer = NULL;
+    bool running;
+
+};
+
+
---
+++ Sdl_Task2_homework/Shapes.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Game.h"
+
+class Shapes {
+
+public:
+
+	static void drawPentagone(SDL_Renderer* gameRenderer, int x, int y, int r);
+
+};
+
+
