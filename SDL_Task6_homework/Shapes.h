f575d276ba35ce82cbde541c3f10b335f7b25974
K4zakov <156944367+K4zakov@users.noreply.github.com>
2024-02-16 08:38:56 +0200

    Task 6 complete

---
+++ SDL_Task6_homework/Shapes.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Game.h"
+
+class Shapes {
+
+public:
+
+	static void drawEmptyRectangle(SDL_Renderer* gameRenderer, int x, int y, int w, int h, int thickness);
+	//static void drawHexagon(SDL_Renderer* gameRenderer, int x, int y, int r);
+
+};
+
+
