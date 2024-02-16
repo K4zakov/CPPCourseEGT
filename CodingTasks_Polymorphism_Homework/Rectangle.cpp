a02d9854acbf4c8b9fed1239a2cde47ca0f8fe07
K4zakov <156944367+K4zakov@users.noreply.github.com>
2024-02-16 12:24:04 +0200

    Delete CodingTasks_Polymorphism_Homework directory

--- CodingTasks_Polymorphism_Homework/Rectangle.cpp
+++
@@ -1,5 +0,0 @@
-#include "Rectangle.h"
-
-void Rectangle::draw() const {
-    cout << "Drawing a " << color << " rectangle with width " << width << " and height " << height << "." << endl;
-}
--- CodingTasks_Polymorphism_Homework/Shape.cpp
+++
@@ -1,5 +0,0 @@
-#include "Shape.h"
-
-void Shape::draw() const {
-    cout << "Drawing a " << color << " shape." <<endl;
-}
